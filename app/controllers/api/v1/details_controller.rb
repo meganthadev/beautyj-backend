8210e27e7f2536d5dd0fd068ef6c87569104e59e
meganthadev <meganthadev@gmail.com>
2020-09-09 23:51:49 -0700

    Add actions to det contr

--- app/controllers/api/v1/details_controller.rb
+++ app/controllers/api/v1/details_controller.rb
@@ -1,2 +1,37 @@
 class Api::V1::DetailsController < ApplicationController
+    before_action :set_product 
+
+
+    class Api::V1::DetailsController < ApplicationController
+
+             
+        def show 
+          @detail = @product.details.find_by(id: params[:id])
+          render json: @detail
+        end 
+        
+        def create 
+            @detail = @product.detail.new(detail_params)
+        end 
+      
+        def update 
+        end 
+        
+        
+        
+        private 
+
+        def set_product
+            @product = Product.find(params[:product_id])
+        end 
+        
+        def detail_params
+            params.require(:detail).permit(:product_id, :description, :ingredients, :full_review)
+        end 
+        
+        
+    end
+        
+
+
 end
