285842a08b8c9af5275d4f988c30caec76e1ae38
meganthadev <meganthadev@gmail.com>
2020-09-09 23:52:15 -0700

    Add actions to prod contr

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,6 +1,36 @@
 class Api::V1::ProductsController < ApplicationController
 
-    
+def index 
+    @products = Product.all 
+    render json: @products
+end 
+
+def create 
+    @product = Product.new(product_params)
+    if @product.save 
+        render json: @product
+    else 
+        render json: {error: 'Error Saving Product'}
+    end 
+end 
+
+def show 
+    @products = Product.find(params[:id])
+    render json: @product
+end 
+
+def destroy 
+    @product = Product.find(params[:id])
+    @product.destroy
+end 
+
+
+
+private 
+
+def product_params
+    params.require(:product).permit(:name, :image_url, :rating)
+end 
 
 
 end
