a42db380980e2396eae38c82068878272c0e87cf
meganthadev <meganthadev@gmail.com>
2020-09-09 19:38:20 -0700

    Namespace api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
 
-  resources :details
-  resources :products
+  namespace :api do 
+    namespace :v1 do
+      resources :details
+      resources :products
+  end 
+end 
+
+ 
+
+  
 end
+
+fetch('http://localhost:3000/api/v1/products')
+fetch('http://localhost:3000/api/v1/details')
