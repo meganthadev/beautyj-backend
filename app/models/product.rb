4602644ed300469db1dfba46100728e89c37f86c
meganthadev <meganthadev@gmail.com>
2020-09-17 20:02:28 -0700

    Remove unnecc code

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,15 +2,9 @@ class Product < ApplicationRecord
     has_one :detail
     validates :name, presence: true, uniqueness: true
 
-    def update_rating(rating)
-        if rating.value == 1...5
-           self.rating = self.rating + rating.amount
-           self.save
-        end 
-    end 
- 
+
 end
 
 
- #Product.update_rating(rating: 5)
+ 
 
